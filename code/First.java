88738faf2a69923b6b628f63f2b59988a8d4d054
subramony <subbufrom@gmail.com>
2017-09-05 16:42:15 +0530

    first java program

---
+++ code/First.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class First
+{
+public static void main(String [] args)
+{
+Scanner scan=new Scanner(System.in);
+System.out.println("please enter user name..");
+String username=scan.next();
+System.out.println("Hello "+username+", How are you ?");
+}
+}
